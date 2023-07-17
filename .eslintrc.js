fbb20ed53a42198d0f818b83fd96f54b39849e7b
seounjin <invanda7@gmail.com>
2023-07-17 18:20:27 +0900

    CHORE : 프로젝트 초기 설정

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  plugins: ['react', 'prettier'],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    'prettier/prettier': 'error',
+  },
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+};
